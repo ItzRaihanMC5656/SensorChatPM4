0f825b95f17363824b2e3729b4a68b26d1d72b89
ItzRaihanMC5656 <78136603+ItzRaihanMC5656@users.noreply.github.com>
2021-12-01 20:48:42 +0700

    Main.php

--- src/RhaxDev/SensorChat/PM4/Main.php
+++ src/RhaxDev/SensorChat/PM4/Main.php
@@ -1,1 +1,30 @@
-Test Aja
+<?php
+
+namespace RhaxDev\SensorChat\PM4;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\event\Event;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+    
+    public function onEnable() : void{
+        $this->saveResource("data.json");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+    public function onChat(PlayerChatEvent $e) {
+        $file = file_get_contents($this->getDataFolder() . "data.json");
+        $json = json_decode($file, true);
+        $f = $json["Format"];
+        foreach($json["List"] as $list){
+            $len = strlen($list);
+            $repeat = str_repeat($f, $len);
+            $replace = str_replace($list, $repeat, $e->getMessage());
+            $e->setMessage($replace);
+        }
+    }
+}
